eeeaec0bc78413e007ce1ac84af4db48b6cde183
euiyeonlee <dmldus1121@naver.com>
2022-11-05 22:01:21 +0900

    Design: Dashboard 레이아웃 및 디자인

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as AsideBar } from "./AsideBar";
+export { default as MainSection } from "./MainSection";
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { AsideBar } from "../components";
+import { AsideBar, MainSection } from "../components";
 
 const Dashboard = () => {
   return (
     <Container>
       <AsideBar />
-      <ContentBox>
-        <Header>icons ... 원티드님</Header>
-        <MainSection>
-          <TitleBox>
-            <Title>대시보드</Title>
-            <DateBox>2022년 11월 5일</DateBox>
-          </TitleBox>
-          <MainBoard>
-            <BoardTitle>통합 광고 현황</BoardTitle>
-            <Board>차트</Board>
-          </MainBoard>
-        </MainSection>
-      </ContentBox>
+      <MainSection />
     </Container>
   );
 };
@@ -30,27 +18,3 @@ const Container = styled.div`
   height: 100vh;
   width: 100vw;
 `;
-
-const ContentBox = styled.section`
-  flex-grow: 1;
-  padding: 0 40px;
-  background-color: ${({ theme }) => theme.colors.grey};
-`;
-
-const Header = styled.div``;
-
-const MainSection = styled.section``;
-
-const TitleBox = styled.div``;
-
-const Title = styled.h1``;
-
-const DateBox = styled.div``;
-
-const MainBoard = styled.div``;
-
-const BoardTitle = styled.h2``;
-
-const Board = styled.div``;
-
-// const  = styled.div``;
