35717c04bcd8bfbc2c23cc80b2c83fbca1d03f5e
euiyeonlee <dmldus1121@naver.com>
2022-11-05 14:26:02 +0900

    Chore: 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Main } from "./pages";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/assets/theme.js
@@ -0,0 +1,31 @@
+const common = {
+  flexCenter: `
+    display: flex;
+    justify-contents: center;
+    align-items: center;
+  `,
+  flexCenterColumn: `
+    display: flex;
+    flex-direction: column;
+    justify-contents: center;
+    align-items: center;
+  `,
+};
+
+const fontSizes = {
+  title: "17px",
+};
+
+const colors = {
+  black: "#000000",
+  grey: "#D9D9D9",
+  white: "#ffffff",
+};
+
+const theme = {
+  common,
+  fontSizes,
+  colors,
+};
+
+export default theme;
